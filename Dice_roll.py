3f3e22ae973ded63fb9cada38802c28ea2c0d8ce
Mukundmurarka <mukundmurarka1996@gmail.com>
2020-05-28 21:53:58 +0530

    first

---
+++ Dice_roll.py
@@ -0,0 +1,55 @@
+import random
+print("It is a rolling dice")
+x = "y"
+while x =="y":
+    number = random.randint(1,6)
+    if number==1:
+        print("----------")
+        print("|         |")
+        print("|         |")
+        print("|    O    |")
+        print("|         |")
+        print("|         |")
+        print("-----------")
+    if number == 2:
+        print("----------")
+        print("|         |")
+        print("|         |")
+        print("| O     O |")
+        print("|         |")
+        print("|         |")
+        print("-----------")
+    if number == 3:
+        print("----------")
+        print("|         |")
+        print("|    O    |")
+        print("|    O    |")
+        print("|    O    |")
+        print("|         |")
+        print("-----------")
+    if number == 4:
+        print("----------")
+        print("|         |")
+        print("| O     O |")
+        print("|         |")
+        print("| O     O |")
+        print("|         |")
+        print("-----------")
+    if number == 5:
+        print("----------")
+        print("|         |")
+        print("| O     O |")
+        print("|    O    |")
+        print("| O     O |")
+        print("|         |")
+        print("-----------")
+    if number == 6:
+        print("----------")
+        print("|         |")
+        print("| O     O |")
+        print("| O     O |")
+        print("| O     O |")
+        print("|         |")
+        print("-----------")
+    x = input("press y for rolling again")
+
